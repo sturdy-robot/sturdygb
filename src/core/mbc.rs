95736abef89b4dc5e8d9b0ced1c654cc90bc5b3d
Pedrenrique Guimaraes <pedrenrique.guimaraes@wdgautomation.com>
2022-08-12 17:56:24 -0300

    add: mbc stubs

--- src/core/mbc.rs
+++ src/core/mbc.rs
@@ -1,3 +1,54 @@
-pub struct MBC {
+pub enum MBCTypes {
+    ROMONLY = 0x00,
+    MBC1 = 0x01,
+    MBC1RAM = 0x02,
+    MBC1RAMBATTERY = 0x03,
+    MBC2 = 0x05,
+    MBC2BATTERY = 0x06,
+    ROMRAM = 0x08,
+    ROMRAMBATTERY = 0x09,
+    MMM01 = 0x0B,
+    MMM01RAM = 0x0C,
+    MMM01RAMBATTERY = 0x0D,
+    MBC3TIMERBATTERY = 0x0F,
+    MBC3TIMERRAMBATTERY = 0x10,
+    MBC3 = 0x11,
+    MBC3RAM = 0x12,
+    MBC3RAMBATTERY = 0x13,
+    MBC5 = 0x19,
+    MBC5RAM = 0x1A,
+    MBC5RAMBATTERY = 0x1B,
+    MBC5RUMBLE = 0x1C,
+    MBC5RUMBLERAM = 0x1D,
+    MBC5RUMBLERAMBATTERY = 0x1E,
+    MBC6 = 0x20,
+    MBC7SENSORRUMBLERAMBATTERY = 0x22,
+    POCKETCAMERA = 0xFC,
+    BANDAITAMA5 = 0xFD,
+    HUC3 = 0xFE,
+    HUC1RAMBATTERY = 0xFF,
+}
+
+pub struct MBC1 {
     
+}
+
+pub struct MBC2 {
+
+}
+
+pub struct MBC3 {
+
+}
+
+pub struct MBC5 {
+
+}
+
+pub struct MBC6 {
+
+}
+
+pub struct MBC7 {
+
 }
