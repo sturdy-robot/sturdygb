33b9d9f4b78473c571d4b4089de0b4bb95e0836b
sturdy-robot <pedrenriquegg@hotmail.com>
2023-02-16 12:24:52 -0300

    initial stubs for ppu

--- src/core/ppu.rs
+++ src/core/ppu.rs
@@ -2,19 +2,89 @@ use super::Memory;
 
 pub struct Ppu {
     lcdc: u8,
+    stat: u8,
+    scy: u8,
+    scx: u8,
+    ly: u8,
+    lyc: u8,
+    dma: u8,
+    bgp: u8,
+    obp0: u8,
+    obp1: u8,
+    wy: u8,
+    wx: u8,
+    key1: u8,
+    vbk: u8,
+    hdma1: u8,
+    hdma2: u8,
+    hdma3: u8,
+    hdma4: u8,
+    hdma5: u8,
+    bcps: u8,
+    bcpd: u8,
+    ocps: u8,
+    ocpd: u8,
+    svbk: u8,
 }
 
 impl Ppu {
     pub fn new() -> Self {
-        Self { lcdc: 0 }
+        Self {
+            lcdc: 0x91,
+            stat: 0x81,
+            scy: 0,
+            scx: 0,
+            ly: 0x91,
+            lyc: 0,
+            dma: 0xFF,
+            bgp: 0xFC,
+            obp0: 0,
+            obp1: 0,
+            wy: 0,
+            wx: 0,
+            key1: 0xFF,
+            vbk: 0xFF,
+            hdma1: 0xFF,
+            hdma2: 0xFF,
+            hdma3: 0xFF,
+            hdma4: 0xFF,
+            hdma5: 0xFF,
+            bcps: 0xFF,
+            bcpd: 0xFF,
+            ocps: 0xFF,
+            ocpd: 0xFF,
+            svbk: 0xFF,
+        }
     }
 }
 
 impl Memory for Ppu {
     fn read_byte(&self, address: u16) -> u8 {
         match address {
-            0x40 => 0xFF, // todo: implement this
-            0x48 => self.lcdc,
+            0xFF40 => self.lcdc,
+            0xFF41 => self.stat,
+            0xFF42 => self.scy,
+            0xFF43 => self.scx,
+            0xFF44 => self.ly,
+            0xFF45 => self.lyc,
+            0xFF46 => self.dma,
+            0xFF47 => self.bgp,
+            0xFF48 => self.obp0,
+            0xFF49 => self.obp1,
+            0xFF4A => self.wy,
+            0xFF4B => self.wx,
+            0xFF4D => self.key1,
+            0xFF4F => self.vbk,
+            0xFF51 => self.hdma1,
+            0xFF52 => self.hdma2,
+            0xFF53 => self.hdma3,
+            0xFF54 => self.hdma4,
+            0xFF55 => self.hdma5,
+            0xFF68 => self.bcps,
+            0xFF69 => self.bcpd,
+            0xFF6A => self.ocps,
+            0xFF6B => self.ocpd,
+            0xFF70 => self.svbk,
             _ => {
                 // println!("PPU Address not implemented {:04X}", address);
                 0xFF
@@ -24,7 +94,31 @@ impl Memory for Ppu {
 
     fn write_byte(&mut self, address: u16, value: u8) {
         match address {
-            _ => (),//println!("PPU Address not implemented {:04X}", address),
-        }
+            0xFF40 => self.lcdc = value,
+            0xFF41 => self.stat = value,
+            0xFF42 => self.scy = value,
+            0xFF43 => self.scx = value,
+            0xFF44 => self.ly = value,
+            0xFF45 => self.lyc = value,
+            0xFF46 => self.dma = value,
+            0xFF47 => self.bgp = value,
+            0xFF48 => self.obp0 = value,
+            0xFF49 => self.obp1 = value,
+            0xFF4A => self.wy = value,
+            0xFF4B => self.wx = value,
+            0xFF4D => self.key1 = value,
+            0xFF4F => self.vbk = value,
+            0xFF51 => self.hdma1 = value,
+            0xFF52 => self.hdma2 = value,
+            0xFF53 => self.hdma3 = value,
+            0xFF54 => self.hdma4 = value,
+            0xFF55 => self.hdma5 = value,
+            0xFF68 => self.bcps = value,
+            0xFF69 => self.bcpd = value,
+            0xFF6A => self.ocps = value,
+            0xFF6B => self.ocpd = value,
+            0xFF70 => self.svbk = value,
+            _ => {},
+        };
     }
 }
