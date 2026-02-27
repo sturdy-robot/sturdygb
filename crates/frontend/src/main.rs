7e800611b7ce0da31c33589781aa39b821f71a7c
Sturdy Robot <pedrenriquegg@hotmail.com>
2026-02-27 01:32:07 -0300

    fix: bind configuration to run_native name
    - This avoid inconsistencies when SturdyGB hits other versions

--- crates/frontend/src/main.rs
+++ crates/frontend/src/main.rs
@@ -56,7 +56,7 @@ fn main() -> eframe::Result<()> {
     }
 
     eframe::run_native(
-        APP_NAME,
+        "sturdygb",
         options,
         Box::new(|cc| Ok(Box::new(app::EmuApp::new(cc, cli.rom)))),
     )
